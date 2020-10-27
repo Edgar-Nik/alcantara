76aefbab5116ca4037b7410f87c9aacc9855d017
Edgar <edgarnikogosian89@gmail.com>
2020-10-27 22:08:28 +0400

    added files

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#banner_1').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+      dots: true
+    });
+    $('#banner_2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        dots:true
+      });
+  });
