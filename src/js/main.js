493fd265af08755e73a727346352e0900106d860
Edgar <edgarnikogosian89@gmail.com>
2020-10-28 17:47:44 +0400

    added new files

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  if($('#banner_1')){
     $('#banner_1').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -6,11 +7,48 @@ $(document).ready(function () {
     //   autoplaySpeed: 2000,
       dots: true
     });
+  }
+   if($('#banner_2')){
     $('#banner_2').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        dots:true
-      });
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      dots:true
+    });
+   }
+
   });
+
+
+  let prodPrice = document.querySelectorAll('.main-price-digit')
+  if(prodPrice.length>0){
+    prodPrice.forEach((item)=>{
+      let curr = item.innerHTML;
+      let result =  curr.split('').reverse().join('').replace(/\d{3}(?=.)/g, '$& ');
+      item.innerHTML = result.split('').reverse().join('')
+    })
+  }
+
+  let menuBtn = document.getElementById('menu')
+  let closeNavBtn = document.getElementById('nav-close-btn') 
+  if(menuBtn){
+    menuBtn.addEventListener('click',()=>{
+      document.body.classList.add('active-main-nav')
+    })
+  }
+
+  if(closeNavBtn){
+    closeNavBtn.addEventListener('click',()=>{
+      document.body.classList.remove('active-main-nav')
+     
+    })
+
+  }
+
+  if( document.querySelector('.main-nav-bg') ){
+    document.querySelector('.main-nav-bg').addEventListener('click',()=>{
+      document.body.classList.remove('active-main-nav')
+
+    })
+  }
